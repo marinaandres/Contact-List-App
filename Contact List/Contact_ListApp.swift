e76f3cf764a5cfdfc7fb3ee444079f6fbe20a14e
marinaandres <maa.andres.aragon@gmail.com>
2023-04-30 15:55:27 +0200

    Initial Commit

---
+++ Contact List/Contact_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contact_ListApp.swift
+//  Contact List
+//
+//  Created by Marina Andrés Aragón on 30/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Contact_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
